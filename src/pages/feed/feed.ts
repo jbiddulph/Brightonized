063035c788a9f682ca9f84c0d298a3fad119abd3
John Biddulph <john.mbiddulph@gmail.com>
2019-02-21 16:13:54 +0000

    likes and comments

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NavController, NavParams, LoadingController, ToastController, ModalController } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, ToastController, ModalController, ActionSheetController, AlertController } from 'ionic-angular';
 import firebase from 'firebase';
 import moment from 'moment';
 import { LoginPage } from '../login/login';
@@ -33,9 +33,10 @@ export class FeedPage {
     private camera: Camera,
     private geolocation: Geolocation,
     private http: HttpClient,
-    private modal: ModalController ) {
+    private modal: ModalController,
+    private actionSheetCtrl: ActionSheetController,
+    private alertCtrl: AlertController ) {
     this.getPosts()
-    
   }
   getPosts() {
     this.posts = []
@@ -46,22 +47,26 @@ export class FeedPage {
     let query = firebase.firestore().collection("posts").orderBy("created", "desc").limit(this.pageSize)
     
     
-    // query.onSnapshot((snapshot) => {
-    //   let changedDocs = snapshot.docChanges();
+    query.onSnapshot((snapshot) => {
+      let changedDocs = snapshot.docChanges();
 
-    //   changedDocs.forEach((change) => {
-    //     if(change.type == "added"){
-    //       //TODO
-    //     }
-    //     if(change.type == "modified"){
-    //       //TODO
-    //       console.log("Document with ID " + change.doc.id + " has been modified")
-    //     }
-    //     if(change.type == "removed"){
-    //       //TODO
-    //     }
-    //   })
-    // })
+      changedDocs.forEach((change) => {
+        if(change.type == "added"){
+          //TODO
+        }
+        if(change.type == "modified"){
+          //TODO
+          for(let i = 0; i < this.posts.length; i++){
+            if(this.posts[i].id == change.doc.id){
+              this.posts[i] = change.doc;
+            }
+          }
+        }
+        if(change.type == "removed"){
+          //TODO
+        }
+      })
+    })
 
     query.get()
     .then((docs) => {
@@ -240,23 +245,35 @@ export class FeedPage {
 
   like(post) {
     let body = {
-      postId: post.Id,
+      postId: post.id,
       userId: firebase.auth().currentUser.uid,
       action: post.data().likes && post.data().likes[firebase.auth().currentUser.uid] == true ? "unlike" : "like"
-    }
-    this.http.post("https://us-central1-quotes-cc05d.cloudfunctions.net/updateTheLikesCount", JSON.stringify(body), {
-      responseType: 'text'
+    } 
+
+    let toast = this.toastCtrl.create({
+      message: "Liking post"
+    });
+    toast.present();
+    // console.log("here is the PostID: ", post.id)
+    // console.log("here is the userID: ", firebase.auth().currentUser.uid)
+    // console.log("here is the PostID: ", post.data().likes && post.data().likes[firebase.auth().currentUser.uid] == true ? 'unlike' : 'like') 
+    this.http.post("https://us-central1-quotes-cc05d.cloudfunctions.net/updateLikesCount", body, {
+      responseType: "text"
     }).subscribe((data) => {
-      console.log("here is the data: "+ data)
+      toast.setMessage("LIKED!")
+      setTimeout(() => {
+        toast.dismiss();
+      }, 3000)
     }, (error) => {
-      console.log("here is the error: "+ error)
+      toast.setMessage("An error has occured, please try again later!")
+      setTimeout(() => {
+        toast.dismiss();
+      }, 3000)
+      console.log("here is the error: ", error)
     })
   }
 
   openModal(post) {
-    
-    
-    
     var docRef = firebase.firestore().collection("posts").doc(post.id)
     docRef.get().then((doc) => {
       if (doc.exists) {
@@ -272,5 +289,62 @@ export class FeedPage {
     }).catch(function(error) {
         console.log("Error getting document:", error);
     });      
+  }
+
+  comment(post) {
+    this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: "View All Comments",
+          handler: () => {
+            //TODO
+          }
+        },
+        {
+          text: "New Comment",
+          handler: () => {
+            this.alertCtrl.create({
+              title: "New Comment",
+              message: "type your message",
+              inputs: [
+                {
+                name: "comment",
+                type: "text"
+                }
+              ],
+              buttons: [
+                {
+                  text: "Cancel",
+              },
+              {
+                text: "Post",
+                handler: (data) => {
+                  if(data.comment){
+                    firebase.firestore().collection("comments").add({
+                      text: data.comment,
+                      post: post.id,
+                      owner: firebase.auth().currentUser.uid,
+                      owner_name: firebase.auth().currentUser.displayName,
+                      created: firebase.firestore.FieldValue.serverTimestamp()
+                    }).then((doc) => {
+                      this.toastCtrl.create({
+                        message: "Comment posted successfully!",
+                        duration: 3000
+                      }).present();
+                    }).catch((err) => {
+                      this.toastCtrl.create({
+                        message: err.message,
+                        duration: 3000
+                      }).present();
+                    })
+                  }
+                }
+              }
+            ]
+            }).present();            
+          }
+        }
+      ]
+    }).present();
   }
 }
